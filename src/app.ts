9588a080d9bbf1717dc76771b8e34d5826a3f263
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-04-20 18:10:15 -0400

    arrow functions

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
+// const button = document.querySelector('button');
+
+// if (button) {
+//     button.addEventListener('click', () => {
+//         console.log('clicked!');
+//     });    
+// }
+
+const userName = 'Marcus';
+let age = 24;
+
+const add = (a: number, b: number) => a + b;
+console.log(add(2, 5));
+
+const printOutput = (output: string | number) => console.log(output);
+
 const button = document.querySelector('button');
 
 if (button) {
-    button.addEventListener('click', () => {
-        console.log('clicked!');
-    });    
+    button.addEventListener('click', event => console.log(event));
 }
+
+printOutput(add(5, 2));
