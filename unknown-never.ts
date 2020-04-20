ce5778115746ee179bd9da7fa925054479b2eea5
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-04-20 10:50:35 -0400

    unknown and never types

---
+++ unknown-never.ts
@@ -0,0 +1,7 @@
+let userInput: unknown;
+
+function generateError(message: string, code: number): never {
+    throw {message: message, errorCode: code};
+}
+
+generateError('An error occurred!', 500);
