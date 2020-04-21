6a7424bd20971cebef7995c7e8e66cf09177d11d
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-04-21 15:29:36 -0400

    getters and setters

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 class Department {
     // private name: string;
-    private employees: string[] = [];
+    protected employees: string[] = [];
 
     constructor(private readonly id: string, public name: string) {
         // this.name = name;
@@ -29,12 +29,37 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+    private lastReport: string;
+
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        throw new Error('No report found.'); // null
+    }
+
+    set mostRecentReport(value: string) {
+        if (!value) {
+            throw new Error('Please pass in a valid value!');
+        }
+        this.addReport(value);
+    }
+
     constructor(id: string, private reports: string[]) {
         super(id, 'Accounting');
+        this.lastReport = reports[0];
+    }
+
+    addEmployee(name: string) {
+        if (name === 'Marcus'){
+            return;
+        }
+        this.employees.push(name);
     }
 
     addReport(text: string) {
         this.reports.push(text);
+        this.lastReport = text;
     }
 
     printReports() {
@@ -50,6 +75,11 @@ it.printEmployeeInformation();
 console.log(it);
 
 const accounting = new AccountingDepartment('d2', []);
+accounting.mostRecentReport = 'Year End Report'; // setter
 accounting.addReport('Something went wrong...');
+console.log(accounting.mostRecentReport);
+accounting.addEmployee('Marcus'); // should not work
+accounting.addEmployee('Chris'); // should work
 accounting.printReports();
+accounting.printEmployeeInformation();
 console.log(accounting);
