0fa0bc9bb4e3e64a4c5386aea61aba5fd8d67047
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-04-22 11:00:12 -0400

    function overloads and optional chaining

--- src/app.ts
+++ src/app.ts
@@ -24,12 +24,12 @@ type Numeric = number | boolean;
 type Universal = Combinable & Numeric;
 
 // Type guards
-function add(a: Combinable, b: Combinable) {
-    if  (typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString();
-    }
-    return a + b;
-}
+// function add(a: Combinable, b: Combinable) {
+//     if  (typeof a === 'string' || typeof b === 'string') {
+//         return a.toString() + b.toString();
+//     }
+//     return a + b;
+// }
 
 type UnknownEmployee = Employee | Admin;
 
@@ -119,3 +119,23 @@ const errorBag: ErrorContainer = {
     username: 'Must start with a capital character!'
 };
 
+// Function overloads
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: Combinable, b: Combinable) {
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+
+const result = add('Marcus', 'Rodrigues');
+
+// Optional chaining
+const fetchedUserData = {
+    id: 'u1',
+    name: 'Marcus',
+    job: { title: 'CEO', description: 'My own company' }
+};
+
+console.log(fetchedUserData?.job?.title);
