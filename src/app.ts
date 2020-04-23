452f3762344db908659c8a4687e38dbf6f3fc4f6
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-04-23 16:10:21 -0400

    created autobind decorator

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,70 @@
+// Autobind decorator.
+function Autobind(
+  _: any,
+  _2: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  };
+  return adjDescriptor;
+}
+
 class ProjectInput {
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLDivElement;
-  element: HTMLFormElement;
+  templateElement: HTMLTemplateElement; // template
+  hostElement: HTMLDivElement; // div
+  element: HTMLFormElement; // form
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
+    // Retrieve form elements.
     this.templateElement = document.getElementById(
       'project-input'
     )! as HTMLTemplateElement;
     this.hostElement = document.getElementById('app')! as HTMLDivElement;
-    
-    // template content
+
+    // Template content.
     const importedNode = document.importNode(
       this.templateElement.content,
       true
     );
+    // Store the form element.
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    // Get access to the form elements.
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    )! as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    )! as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      '#people'
+    )! as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
+  @Autobind
+  private submitHandler(event: Event) {
+    event.preventDefault(); // disable HTTP request
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler);
+  }
+
+  // Render an element (in this case, the form) to the div.
   private attach() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
   }
