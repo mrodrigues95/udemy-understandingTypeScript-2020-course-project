90c1d3b9b68c71453378e26176a6b397a788531c
mrodrigues95 <marcus.rodrigues95@gmail.com>
2020-04-22 20:53:19 -0400

    custom bind decorator

--- src/app.ts
+++ src/app.ts
@@ -8,15 +8,21 @@ function Logger(logString: string) {
 
 function WithTemplate(template: string, hookId: string) {
     console.log('TEMPLATE FACTORY');
-    return function(constructor: any) {
-        console.log('Rendering tamplate...');
-        const hookEl = document.getElementById(hookId);
-        const p = new constructor();
-        if (hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector('h1')!.textContent = p.name;
+    return function<T extends { new(...args: any[]): {name:string} }>(
+        originalConstructor: T
+        ) {
+        return class extends originalConstructor {
+            constructor(..._: any[]) {
+                super();
+                console.log('Rendering tamplate...');
+                const hookEl = document.getElementById(hookId);
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector('h1')!.textContent = this.name;
+                }
+            }
         }
-    }
+    };
 }
 
 @Logger('LOGGING')
@@ -30,7 +36,6 @@ class Person {
 }
 
 const pers = new Person();
-
 console.log(pers);
 
 // ---
@@ -83,3 +88,30 @@ class Product {
         return this._price * (1 + tax);
     }
 }
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
+class Printer {
+    message = 'This works!';
+
+    @Autobind
+    showMessage() {
+        console.log(this.message);
+    }
+}
+
+const p = new Printer();
+
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage);
